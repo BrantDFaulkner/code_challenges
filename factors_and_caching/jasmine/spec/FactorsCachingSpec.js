fe2214ab7a891b8fab9b34da347cfea0fe0ff4c2
BrantDFaulkner <brantdfaulkner@gmail.com>
2016-01-10 16:02:55 -0600

    implemented mock jasmine test

---
+++ factors_and_caching/jasmine/spec/FactorsCachingSpec.js
@@ -0,0 +1,7 @@
+describe("FactorsCaching", function() {
+
+    it("should have a numProp of 2", function() {
+      expect(myObject.numProp).toBe(2);
+    });
+
+});
---
+++ factors_and_caching/jasmine/src/FactorsCaching.js
@@ -0,0 +1,9 @@
+
+
+
+
+var myObject = {
+    sProp: 'some string value',
+    numProp: 2,
+    bProp: false
+};
