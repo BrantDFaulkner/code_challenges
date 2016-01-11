939bd3f7af4dd2542cfd73c452d7fad5fedc8835
BrantDFaulkner <brantdfaulkner@gmail.com>
2016-01-10 18:30:57 -0600

    add findFactors with spec

--- factors_and_caching/jasmine/spec/FactorsCachingSpec.js
+++ factors_and_caching/jasmine/spec/FactorsCachingSpec.js
@@ -1,7 +1,35 @@
 describe("FactorsCaching", function() {
+  describe("findFactors", function () {
+    describe("maxFactor", function() {
+      it("should find the largest potential factor", function() {
+          expect(maxFactor(10)).toBe(5);
+          expect(maxFactor(99)).toBe(49);
+      });
+    })//maxFactor;
 
-    it("should have a numProp of 2", function() {
-      expect(myObject.numProp).toBe(2);
+    it("should return an array of factors", function() {
+      expect(findFactors(10)).toEqual([5,2])
+      expect(findFactors(20)).toEqual([10,5,4,2])
     });
 
-});
+
+  });//findFactors
+});//FactorsCaching
+
+
+// The 'toBe' matcher compares with ===
+// The 'toEqual' matcher
+// works for simple literals and variables
+// should work for objects
+// The 'toMatch' matcher is for regular expressions
+// The 'toBeDefined' matcher compares against `undefined`
+// The `toBeUndefined` matcher compares against `undefined`
+// The 'toBeNull' matcher compares against null
+// The 'toBeTruthy' matcher is for boolean casting testing
+// The 'toBeFalsy' matcher is for boolean casting testing
+// The 'toContain' matcher is for finding an item in an Array
+// The 'toBeLessThan' matcher is for mathematical comparisons
+// The 'toBeGreaterThan' matcher is for mathematical comparisons
+// The 'toBeCloseTo' matcher is for precision math comparison
+// The 'toThrow' matcher is for testing if a function throws an exception
+// The 'toThrowError' matcher is for testing a specific thrown exception
--- factors_and_caching/jasmine/src/FactorsCaching.js
+++ factors_and_caching/jasmine/src/FactorsCaching.js
@@ -1,9 +1,26 @@
 
+var maxFactor = function(num) {
+  return Math.floor(num/2)
+};
+
+// for (i = 0; i < cars.length; i++) {
+//     text += cars[i] + "<br>";
+// }
+
+// if (condition) {
+//     block of code to be executed if the condition is true
+// } else {
+//     block of code to be executed if the condition is false
+// }
 
 
-
-var myObject = {
-    sProp: 'some string value',
-    numProp: 2,
-    bProp: false
+var findFactors = function(num) {
+  var max = maxFactor(num)
+  var factors = []
+  for (i=2; i <= max; i++) {
+    if (num % i == 0) {
+      factors.unshift(i)
+    }
+  }
+  return factors
 };
